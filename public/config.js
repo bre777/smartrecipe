7893fcc116344f4328391b5749c6a1950272b86c
Breanna Loughrey <loughrey-b1@ulster.ac.uk>
2026-01-07 20:43:04 +0000

    fix

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-window.API_BASE = "https://YOUR-FUNCTION-APP.azurewebsites.net/api";
+window.API_BASE = "https://smartrecipe-api-c2h0f5gdgcbgc3cx.uksouth-01.azurewebsites.net/api";
 window.AUTHOR_ID = "demo-user";
